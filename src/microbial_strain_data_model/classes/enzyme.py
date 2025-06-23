8ee831982cd7f183d6de5005e05d5984b6c9d6b1
juwitte <juwi93@gmx.de>
2025-06-23 08:49:36 +0000

    feat: add relationData to Enzyme and OxygenRelation

--- src/microbial_strain_data_model/classes/enzyme.py
+++ src/microbial_strain_data_model/classes/enzyme.py
@@ -1,7 +1,7 @@
 from typing_extensions import Annotated
 from pydantic import BaseModel, ConfigDict, StringConstraints, Field, HttpUrl
 
-from microbial_strain_data_model.classes.links import SourceLink
+from microbial_strain_data_model.classes.links import RelationLink, SourceLink
 
 
 class Enzyme(BaseModel):
@@ -27,6 +27,11 @@ class Enzyme(BaseModel):
     url: HttpUrl | None = Field(default=None, title="URL", description="URL")
     alternateName: list[str] = Field(default_factory=list, title="Alternate Name")
     active: bool | None = Field(default=None, title="Active")
+    relatedData: list[RelationLink] = Field(
+        default_factory=list,
+        title="Related Data",
+        description="JSON paths to relation object",
+    )
     source: list[SourceLink] = Field(
         title="Source", description="List of JSON paths to source object"
     )
