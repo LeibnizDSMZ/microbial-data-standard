4fde0496c47d634e4f83af9493c6e650c9b6d061
juwitte <juwi93@gmx.de>
2025-07-30 11:10:31 +0000

    feat(source): new source class

--- src/microbial_strain_data_model/classes/links.py
+++ src/microbial_strain_data_model/classes/links.py
@@ -3,5 +3,8 @@ from typing_extensions import Annotated
 from pydantic import StringConstraints
 
 
-SourceLink = Annotated[str, StringConstraints(pattern=r"^\/sources\/\d+$")]
+SourceLink = Annotated[
+    str,
+    StringConstraints(pattern=r"^\/sources\/\d+$"),
+]
 RelationLink = Annotated[str, StringConstraints(pattern=r"^\/relatedData\/\d+$")]
