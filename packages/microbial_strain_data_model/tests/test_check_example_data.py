f2cc5e1a443ed5c1674cfb611a8f1074ca514ace
artdotlis <artdotlis@pm.me>
2026-06-09 18:03:50 +0000

    test: use file-relative path for test data loading

--- packages/microbial_strain_data_model/tests/test_check_example_data.py
+++ packages/microbial_strain_data_model/tests/test_check_example_data.py
@@ -1,7 +1,14 @@
 from pathlib import Path
 from microbial_strain_data_model.strain import Strain
 
+_ROOT = Path(__file__).parent
+
 
 def test_validate_bacteria_example() -> None:
-    with Path("./tests/example_data/bacteria.json").open("r") as f_in:
+    with _ROOT.joinpath("example_data/bacteria.json").open("r") as f_in:
         Strain.model_validate_json(f_in.read())
+
+
+# def test_validate_fungi_example() -> None:
+#    with _ROOT.joinpath("example_data/fungi.json").open("r") as f_in:
+#        Strain.model_validate_json(f_in.read())
