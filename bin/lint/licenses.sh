80f14679c87aa6454f021052275c0d2d948ef6ce
Artur Lissin <artdotlis@pm.me>
2026-06-18 13:21:02 +0000

    ci: improve license authors

--- bin/lint/licenses.sh
+++ bin/lint/licenses.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# SPDX-FileCopyrightText: 2026 Artur Lissin
+# SPDX-FileCopyrightText: 2026 Leibniz Institute DSMZ-German Collection of Microorganisms and Cell Cultures GmbH
 #
 # SPDX-License-Identifier: Unlicense
 
